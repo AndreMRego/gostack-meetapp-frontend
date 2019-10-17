42377a74cc91062ce1ff4eb8a801fd22f0fa7abd
Andre Work <andremregodev@gmail.com>
2019-10-17 17:37:48 -0300

    configurando store com redux, saga , tostify e reactotron

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react'
+import { PersistGate } from 'redux-persist/integration/react'
+import { ToastContainer } from 'react-toastify'
 import { Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import './config/ReactotronConfig'
 
+import GlobalStyle from './styles/global'
 import Routes from './routes'
+
 import history from './services/history'
 
-import GlobalStyle from './styles/global'
+import { store, persistor } from './store'
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+          <ToastContainer autoClose={3000} />
+        </Router>
+      </PersistGate>
+    </Provider>
   )
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import SignIn from '~/pages/SignIn'
 import SignUp from '~/pages/SignUp'
 
 import Dashboard from '~/pages/Dashboard'
+
 import Profile from '~/pages/Profile'
 
 export default function routes() {
